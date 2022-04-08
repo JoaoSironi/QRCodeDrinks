ae099ce565d909d32a7e46592d32bbeb4e191bca
Joao Sironi <joaovsironi@gmail.com>
2022-04-08 18:18:55 -0300

    feat(project): started the code

---
+++ src/drinksAPI/Controllers/DrinksController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace drinksAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DrinksController
+    {
+        private readonly ILogger<DrinksController> _logger;
+
+        public DrinksController(ILogger<DrinksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IList<Models.Drink> DrinksList()
+        {
+            return new List<Models.Drink>();
+        }
+
+        [HttpGet("{id}")]
+        public IList<Models.Drink> FindDrink(string id)
+        {
+            return new List<Models.Drink>();
+        }
+
+        [HttpPost]
+        public IList<Models.Drink> CreateDrink()
+        {
+            return new List<Models.Drink>();
+        }
+
+        [HttpPut("{id}")]
+        public IList<Models.Drink> UpdateDrink(string id)
+        {
+            return new List<Models.Drink>();
+        }
+
+
+        [HttpDelete("{id}")]
+        public IList<Models.Drink> DeleteDrink(string id)
+        {
+            return new List<Models.Drink>();
+        }
+    }
+}
---
+++ src/drinksAPI/Models/Drink.cs
@@ -0,0 +1,19 @@
+﻿namespace drinksAPI.Models
+{
+    public record Drink(
+        string uuid,
+        string name,
+        string brand,
+        string country,
+        string state,
+        string type,
+        string subType,
+        int age,
+        int year,
+        string visual,
+        string scents,
+        string taste,
+        Array harmonization,
+        string observation
+    );
+}
